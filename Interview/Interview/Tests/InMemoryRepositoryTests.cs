4d0a00be06a42efe811b8f9846a314722f560a5b
Damian Kingsbury <damiank@objectteam.com>
2020-02-21 12:15:47 +0000

    Refactored to use Dictionary initializer

--- Interview/Interview/Tests/InMemoryRepositoryTests.cs
+++ Interview/Interview/Tests/InMemoryRepositoryTests.cs
@@ -25,10 +25,12 @@ namespace Interview.Tests
         [Test]
         public void GetAll_ReturnsAllItems()
         {
-            var items = new Dictionary<int, IStoreable<int>>();
-            items[1] = new TestStoreable { Id = 1 };
-            items[2] = new TestStoreable { Id = 2 };
-            items[3] = new TestStoreable { Id = 3 };
+            var items = new Dictionary<int, IStoreable<int>>
+            {
+                [1] = new TestStoreable { Id = 1 },
+                [2] = new TestStoreable { Id = 2 },
+                [3] = new TestStoreable { Id = 3 }
+            };
             var inMemoryRepository = new InMemoryRepository<IStoreable<int>, int>(items);
 
             var actual = inMemoryRepository.GetAll();
